b676a46b0205dd474e9cf39405776af10dfe84d0
bibeast <160469535+bibeast@users.noreply.github.com>
2024-09-20 11:12:26 -0500

    db changes

--- models/db.js
+++ models/db.js
@@ -1,9 +1,10 @@
 const { Client } = require("pg");
 module.exports.client = new Client({
   // naming export system
-  user: "postgres",
-  host: "localhost",
-  database: "Fitness",
-  password: "Skater28!",
+  user: "fitness_app_hxwd_user",
+  host: "dpg-crmpe82j1k6c739bqcig-a.oregon-postgres.render.com",
+  database: "fitness_app_hxwd",
+  password: "DJ6p0UifNdhTglGcxuVKsUqLKe5f2zq4",
   port: 5432,
+  ssl: { rejectUnauthorized: false },
 });
